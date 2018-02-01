ccd60be1679977c6a938e52271102b2a500462fd
Rumenigue Nogueira da Silva <niguerag@hotmail.com>
2018-01-31 22:07:48 -0200

    Building nested components done

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -36,4 +36,8 @@ export class ProductListComponent {
     toggleImage(): void {
       this.showImage = !this.showImage;
     }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
 }
